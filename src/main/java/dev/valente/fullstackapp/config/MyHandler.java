9733cac984f3836c2364a7b548c5efeca9b56ba2
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-11-22 21:20:38 -0300

    CONSEGUI

---
+++ src/main/java/dev/valente/fullstackapp/config/MyHandler.java
@@ -0,0 +1,20 @@
+package dev.valente.fullstackapp.config;
+
+import com.google.gson.Gson;
+import dev.valente.fullstackapp.model.ChatInput;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+public class MyHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        String payload = message.getPayload();
+        Gson gson = new Gson();
+        ChatInput user = gson.fromJson(payload, ChatInput.class);
+        session.sendMessage(new TextMessage(user.user() + " : " + user.message()));
+    }
+}

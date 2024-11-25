6c9ab220f045c9dc988cad824a5a0b844e0cfeb8
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-11-25 17:17:57 -0300

    add: Separando responsabilidades

--- src/main/java/dev/valente/fullstackapp/config/MyHandler.java
+++ src/main/java/dev/valente/fullstackapp/config/MyHandler.java
@@ -2,31 +2,58 @@ package dev.valente.fullstackapp.config;
 
 import com.google.gson.Gson;
 import dev.valente.fullstackapp.model.ChatInput;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.adapter.standard.StandardWebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.util.HtmlUtils;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class MyHandler extends TextWebSocketHandler {
+public class MyHandler extends AbstractWebSocketHandler {
 
-    protected void handleTextMessage(WebSocketSession session, TextMessage message, HttpServletRequest request) throws IOException {
+    //UUID da sessão, precisa ser trocado por um UUID de Usuário
+    private final Map<UUID, WebSocketSession> userSessions = new ConcurrentHashMap<>();
+
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+
+        //StandardWebSocketSession
         String payload = message.getPayload();
         HttpHeaders httpHeaders = session.getHandshakeHeaders();
-        HttpSession httpSession = request.getSession();
         InetSocketAddress address = session.getRemoteAddress();
-        String idSesssion = session.getId();
         String ipUser = httpHeaders.getOrigin();
 
+        Gson gson = new Gson();
+        ChatInput user = gson.fromJson(payload, ChatInput.class);
+        session.sendMessage(new TextMessage(HtmlUtils.htmlEscape(user.user() + " : " + user.message())));
+    }
 
 
-//        Gson gson = new Gson();
-//        ChatInput user = gson.fromJson(payload, ChatInput.class);
-        session.sendMessage(new TextMessage(HtmlUtils.htmlEscape(payload)));
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        userSessions.put(UUID.fromString(session.getId()), session);
+        System.out.println("Sessão com ID: " + session.getId() + " foi aberta e adicionada ao HashMap");
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        try(var teste = userSessions.remove(UUID.fromString(session.getId()))){
+            System.out.println("Sessão com ID: " + session.getId() + " foi fechada e excluída do HashMap");
+        } catch (Exception error){
+            System.err.println(error.getMessage());
+        }
+
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
     }
 }

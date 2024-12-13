9970e37fa50ba7d06e247015e3f178404cb4fcda
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-12-13 19:29:57 -0300

    feat: Create NickName and persist

--- src/main/java/dev/valente/fullstackapp/model/NickName.java
+++ src/main/java/dev/valente/fullstackapp/model/NickName.java
@@ -5,35 +5,57 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.util.UUID;
+import java.util.Objects;
 
 @Entity
 public class NickName {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String nickName;
 
-    public NickName(UUID id, String nickName) {
-        this.id = id;
-        this.nickName = nickName;
-    }
-
-    public NickName(String nickName) {
+    public NickName( String nickName) {
         this.nickName = nickName;
     }
 
     public NickName() {
     }
 
-    public UUID getId() {
+    public Integer getId() {
         return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getNickName() {
         return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        NickName nickName1 = (NickName) o;
+        return Objects.equals(id, nickName1.id) && Objects.equals(nickName, nickName1.nickName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nickName);
+    }
+
+    @Override
+    public String toString() {
+        return "NickName{" +
+                "id=" + id +
+                ", nickName='" + nickName + '\'' +
+                '}';
     }
 }
--- src/main/java/dev/valente/fullstackapp/repository/NickNameRepository.java
+++ src/main/java/dev/valente/fullstackapp/repository/NickNameRepository.java
@@ -2,11 +2,6 @@ package dev.valente.fullstackapp.repository;
 
 import dev.valente.fullstackapp.model.NickName;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
-@Repository
-public interface NickNameRepository extends JpaRepository<NickName, UUID> {
-
+public interface NickNameRepository extends JpaRepository<NickName, Integer> {
 }
